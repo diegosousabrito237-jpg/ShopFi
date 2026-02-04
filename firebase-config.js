1afa033fa12101f6a32498abba75c2d5d4109bd3
diegosousabrito237-jpg <diegosousabrito237@gmail.com>
2026-02-04 17:34:23 -0300

    firebase-config.js
    Updated firebase configuration to hide sensitive information.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCPJ4MFzWJ6f9hYSytqVsNtK9GAYwNs4",
-  authDomain: "shopfi-br.firebaseapp.com",
-  projectId: "shopfi-br",
-  storageBucket: "shopfi-br.firebasestorage.app",
-  messagingSenderId: "430527615562",
-  appId: "1:430527615562:web:7d14f09dca8959fe318cb3"
+const firebaseConfig = {
+  apiKey: "....",
+  authDomain: "....",
+  projectId: "....",
+  storageBucket: "....",
+  messagingSenderId: "....",
+  appId: "...."
 };
