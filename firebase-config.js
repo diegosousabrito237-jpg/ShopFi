37d991b3b673b42abd7c79df8c71d8395f33d05e
diegosousabrito237-jpg <diegosousabrito237@gmail.com>
2026-02-04 16:52:04 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
